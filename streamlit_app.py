e1ea912b4bb6450012e27681f116b6f4c9eff192
guideleon <116895044+guideleon@users.noreply.github.com>
2024-01-24 14:14:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-#import run_model_py
+
 """
+import run_model_py
 # Welcome to Streamlit!
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.

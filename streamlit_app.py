b760c4a037741b1e2026f58c6d91b3a3cc72b641
guideleon <116895044+guideleon@users.noreply.github.com>
2024-01-24 14:31:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import run_model_py
+import pickle
+model=pickle.load(open('model.pkl', 'rb'))
 
 st.title("Ingresar Datos para Predicción")
 

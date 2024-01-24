055cae4241d521cd62d40db3d84487531fa4a855
guideleon <116895044+guideleon@users.noreply.github.com>
2024-01-24 13:48:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-run run_model_py.py
+!streamlit run run_model_py.py
